0d2add434d1222cef694cd7ac8a63d3f5d4ceeb5
shunichi-h <61550279+shunichi-h@users.noreply.github.com>
2020-03-08 14:59:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+$(function(){
 
+//common
+
+//header
+$('header a').click(function(){
+  var id = $(this).attr('href');
+  var position = $(id).offset().top;
+  $('html, body').animate({
+    'scrollTop': position
+  }, 500);
+});
+
+//top-wrapper
+
+//profile-wrapper
+
+//skill-wrapper
+
+//contact-wrapper
+$('.contact-form').click(function(){
+  $('.g-form').slideDown();
+});
+
+$('.fa-2x').click(function(){
+  $('.g-form').slideUp();
+});
+
+//pagetop-wrapper
+$('#top-btn').click(function(){
+  $('html, body').animate({
+    'scrollTop':0
+  }, 500);
+});
+
+//footer
+
+
+});
