342bf85b28b904ed851ab3e5def9da6a84a3c990
shunichi-h <61550279+shunichi-h@users.noreply.github.com>
2021-01-11 12:28:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(function(){
 
 //common
+$(function(){
+	$(window).scroll(function (){
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 100){
+				$(this).addClass('scrollin');
+			}
+		});
+	});
+});
 
 //header
 $('header a').click(function(){
